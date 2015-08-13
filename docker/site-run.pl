f2862b1615fef3c8ee961b8e34408459fb3438f1
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2015-08-13 11:30:37 +0300

    Added site assembly

---
+++ docker/site-run.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+# Workaround SIGTERM not being handled by bash
+$SIG{TERM} = sub {
+  die "exiting site...\n"
+};
+system "/usr/local/bin/node --harmony server.js /etc/extendedmind/site/filosofianakatemia-config.js";
