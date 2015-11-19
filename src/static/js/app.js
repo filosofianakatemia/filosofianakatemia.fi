3e655c89a68939467a3454f2a005433d362dcb24
JP Salo <jukapeka@gmail.com>
2015-11-19 17:16:36 +0200

    Fetch social media share count in a single blog post page. Hide counter from blog posts list.

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -37,8 +37,53 @@
     event.preventDefault();
     var sharelUrl = 'https://filosofianakatemia.fi/blogi/' + path;
     var url = 'https://www.linkedin.com/shareArticle?mini=true&url=' + sharelUrl + '&title=' + title +
-      '&summary=' + ingress + '&source=Filosofian%20Akatemia';
+    '&summary=' + ingress + '&source=Filosofian%20Akatemia';
     window.open(url, '_blank', 'height=420, width=550');
+  };
+
+  function httpGetAsync(theUrl, callback)
+  {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        callback(xmlHttp.responseText);
+    };
+    xmlHttp.open('GET', theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+  }
+
+  app.getShareCount = function(path) {
+    // http://stackoverflow.com/a/19119580
+    var totalCount = 0;
+    var totalCountElement = document.getElementById('total-shares_count');
+    var sharelUrl = 'https://filosofianakatemia.fi/blogi/' + path;
+
+    var facebookUrl = 'https://api.facebook.com/method/links.getStats?urls=' + sharelUrl + '&format=json';
+    httpGetAsync(facebookUrl, facebookCount);
+    function facebookCount(response) {
+      totalCount += JSON.parse(response)[0].share_count;
+      if (totalCountElement) {
+        totalCountElement.innerHTML = totalCount;
+      }
+    }
+
+    var twitterUrl = 'http://cdn.api.twitter.com/1/urls/count.json?url=' + sharelUrl;
+    httpGetAsync(twitterUrl, twitterCount);
+    function twitterCount(response) {
+      totalCount += JSON.parse(response).count;
+      if (totalCountElement) {
+        totalCountElement.innerHTML = totalCount;
+      }
+    }
+
+    var linkedInUrl = 'http://www.linkedin.com/countserv/count/share?url=' + sharelUrl + '&format=json';
+    httpGetAsync(linkedInUrl, linkedInCount);
+    function linkedInCount(response) {
+      totalCount += JSON.parse(response).count;
+      if (totalCountElement) {
+        totalCountElement.innerHTML = totalCount;
+      }
+    }
   };
 
 })(window.app = window.app || {});
