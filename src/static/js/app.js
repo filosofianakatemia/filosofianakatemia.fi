6071107d79b914e0db5226ff696c9533976637e4
JP Salo <jukapeka@gmail.com>
2015-11-19 08:44:23 +0200

    Fixed broken Facebook share and Twitter tweet blog post links.

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -14,7 +14,7 @@
     FB.ui(
     {
       method: 'share',
-      href: 'https://filosofianakatemia.fi/blogi' + path,
+      href: 'https://filosofianakatemia.fi/blogi/' + path,
       picture: pictureUrl,
       title: title,
       description: ingress
@@ -26,7 +26,7 @@
 
   app.twitterShareBlog = function(path, title) {
     // http://gpiot.com/blog/elegant-twitter-share-button-and-dialog-with-jquery/
-    var sharelUrl = 'https://filosofianakatemia.fi/blogi' + path;
+    var sharelUrl = 'https://filosofianakatemia.fi/blogi/' + path;
     var shareVia = 'filosofianakate';
     var url = 'http://twitter.com/share?via=' + shareVia + '&amp;url=' + sharelUrl + '&amp;text=' + title;
     window.open(url, '_blank', 'height=420, width=550');
