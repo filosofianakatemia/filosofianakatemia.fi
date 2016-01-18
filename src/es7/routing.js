4245ae354fd3ed2ef6b87e3d207f91aaca11b8d6
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-01-18 15:01:21 +0200

    Added support for Youtube video and #devblogi shown when debugging. Added Filosofian Akatemia author to group effort blog posts. Miscallaneous fixes here and there.

--- src/es7/routing.js
+++ src/es7/routing.js
@@ -69,24 +69,164 @@ module.exports = (config, app, backendApi) => {
   markdownParser.use(markdownParserContainer, 'right-align', {render: rightContainerRender});
   function leftContainerRender(tokens, idx) {
     if (tokens[idx].nesting === 1) {
-        // opening tag
-        return '<div class="large-5 large-offset-1 left-aligned columns">\n';
-      } else {
-        // closing tag
-        return '</div>\n';
-      }
+      // opening tag
+      return '<div class="large-5 large-offset-1 left-aligned columns">\n';
+    } else {
+      // closing tag
+      return '</div>\n';
     }
-    function rightContainerRender(tokens, idx) {
-      if (tokens[idx].nesting === 1) {
-        // opening tag
-        return '<div class="large-5 columns end person-details">' +
-        '<div class="show-for-large-up"><br/><br/></div>\n';
-      } else {
-        // closing tag
-        return '</div>\n';
+  }
+  function rightContainerRender(tokens, idx) {
+    if (tokens[idx].nesting === 1) {
+      // opening tag
+      return '<div class="large-5 columns end person-details">' +
+      '<div class="show-for-large-up"><br/><br/></div>\n';
+    } else {
+      // closing tag
+      return '</div>\n';
+    }
+  }
+
+  // Youtube (and non-tested Vimeo)
+
+  // The youtube_parser is from http://stackoverflow.com/a/8260383
+  function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match&&match[7].length==11){
+      return match[7];
+    } else{
+      return url;
+    }
+  }
+
+  // The vimeo_parser is from http://stackoverflow.com/a/13286930
+  function vimeo_parser(url){
+    var regExp = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/;
+    var match = url.match(regExp);
+    if (match){
+      return match[3];
+    } else{
+      return url;
+    }
+  }
+
+  function video_embed(md) {
+    function video_return(state, silent) {
+      var code,
+          serviceEnd,
+          serviceStart,
+          pos,
+          res,
+          videoID = '',
+          tokens,
+          token,
+          start,
+          oldPos = state.pos,
+          max = state.posMax;
+
+      // When we add more services, (youtube) might be (youtube|vimeo|vine), for example
+      var EMBED_REGEX = /@\[(youtube|vimeo)\]\([\s]*(.*?)[\s]*[\)]/im;
+
+
+      if (state.src.charCodeAt(state.pos) !== 0x40/* @ */) {
+        return false;
       }
+      if (state.src.charCodeAt(state.pos + 1) !== 0x5B/* [ */) {
+        return false;
+      }
+
+      var match = EMBED_REGEX.exec(state.src);
+
+      if(!match){
+        return false;
+      }
+
+      if (match.length < 3){
+        return false;
+      }
+
+
+      var service = match[1];
+      var videoID = match[2];
+      if (service.toLowerCase() == 'youtube') {
+        videoID = youtube_parser(videoID);
+      } else if (service.toLowerCase() == 'vimeo') {
+        videoID = vimeo_parser(videoID);
+      }
+
+      // If the videoID field is empty, regex currently make it the close parenthesis.
+      if (videoID === ')') {
+        videoID = '';
+      }
+
+      serviceStart = state.pos + 2;
+      serviceEnd = md.helpers.parseLinkLabel(state, state.pos + 1, false);
+
+      //
+      // We found the end of the link, and know for a fact it's a valid link;
+      // so all that's left to do is to call tokenizer.
+      //
+      if (!silent) {
+        state.pos = serviceStart;
+        state.posMax = serviceEnd;
+        state.service = state.src.slice(serviceStart, serviceEnd);
+        var newState = new state.md.inline.State(
+          service,
+          state.md,
+          state.env,
+          tokens = []
+        );
+        newState.md.inline.tokenize(newState);
+
+        token = state.push('video', '');
+        token.videoID = videoID;
+        token.service = service;
+        token.level = state.level;
+      }
+
+      state.pos = state.pos + state.src.indexOf(')');
+      state.posMax = state.tokens.length;
+      return true;
     }
 
+    return video_return;
+  }
+
+  function tokenize_youtube(videoID) {
+    var embedStart = '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" id="ytplayer" type="text/html" width="640" height="390" src="//www.youtube.com/embed/';
+    var embedEnd = '" frameborder="0"></iframe></div>';
+    return embedStart + videoID + embedEnd;
+  }
+
+  function tokenize_vimeo(videoID) {
+    var embedStart = '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" id="vimeoplayer" width="500" height="281" src="//player.vimeo.com/video/';
+    var embedEnd = '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
+    return embedStart + videoID + embedEnd;
+  }
+
+  function tokenize_video(md) {
+    function tokenize_return(tokens, idx, options, env, self) {
+        var videoID = md.utils.escapeHtml(tokens[idx].videoID);
+        var service = md.utils.escapeHtml(tokens[idx].service);
+        if (videoID === '') {
+            return '';
+        }
+
+      if (service.toLowerCase() === 'youtube') {
+        return tokenize_youtube(videoID);
+      } else if (service.toLowerCase() === 'vimeo') {
+          return tokenize_vimeo(videoID);
+      } else{
+         return('');
+      }
+
+    }
+
+    return tokenize_return;
+  }
+  markdownParser.renderer.rules.video = tokenize_video(markdownParser);
+  markdownParser.inline.ruler.before('emphasis', 'video', video_embed(markdownParser));
 
   // ROUTE MIDDLEWARE
 
@@ -191,44 +331,35 @@ module.exports = (config, app, backendApi) => {
   async function blogiTeksti(ctx, path) { // jshint ignore:line
     console.log('GET /blogi/' + path);
     let faPublicItems = await backendClient.getPublicItems('filosofian-akatemia');  // jshint ignore:line
-    let publicNote = faPublicItems.getNote(path);
-
-    if (publicNote && publicNote.keywords && publicNote.keywords.length) {
-      let keywordFound = false;
-      for (let i = 0; i < publicNote.keywords.length; i++) {
-        if (publicNote.keywords[i].title === 'blogi') {
-          keywordFound = true;
-          break;
-        } else if (publicNote.keywords[i].parent) {
-          let parentTag = faPublicItems._getTagByUUID(publicNote.keywords[i].parent);
-          if (parentTag && parentTag.title === 'blogi') {
-            keywordFound = true;
-            break;
-          }
-        }
+    let blogPosts = filterBlogsFromPublicItems(faPublicItems);
+    let blogPost;
+    for (let i = 0; i < blogPosts.length; i++) {
+      if (blogPosts[i].visibility && blogPosts[i].visibility.path === path) {
+        blogPost = blogPosts[i];
+        break;
       }
-      if (keywordFound) {
-        let blog = renderBlogPost(publicNote);
-        for (let i=0; i<publicNote.keywords.length; i++) {
-          if (isAuthorTag(publicNote.keywords[i])) {
-            // Get the introduction of the author.
-            let personDescriptionPath = getAuthorDescriptionPath(publicNote.keywords[i].title);
-            let personDescriptionNote = faPublicItems.getNote(personDescriptionPath);
-            if (personDescriptionNote){
-              if (!blog.author) {
-                blog.author = {};
-              }
-              blog.author.description = markdownParser.render(personDescriptionNote.content);
-              blog.author.pictureSource = getAuthorThumbnailPath(publicNote.keywords[i].title);
+    }
+    if (blogPost) {
+      let blog = renderBlogPost(blogPost);
+      for (let i=0; i<blogPost.keywords.length; i++) {
+        if (isAuthorTag(blogPost.keywords[i])) {
+          // Get the introduction of the author.
+          let personDescriptionPath = getAuthorDescriptionPath(blogPost.keywords[i].title);
+          let personDescriptionNote = faPublicItems.getNote(personDescriptionPath);
+          if (personDescriptionNote){
+            if (!blog.author) {
+              blog.author = {};
             }
-            break;
+            blog.author.description = markdownParser.render(personDescriptionNote.content);
+            blog.author.pictureSource = getAuthorThumbnailPath(blogPost.keywords[i].title);
           }
+          break;
         }
-        let context = {
-          blog: blog
-        };
-        ctx.body = render('pages/blogiteksti', context);
       }
+      let context = {
+        blog: blog
+      };
+      ctx.body = render('pages/blogiteksti', context);
     }
   }
 
@@ -362,7 +493,17 @@ module.exports = (config, app, backendApi) => {
 
   async function getUnrenderedBlogs(){
     let faPublicItems = await backendClient.getPublicItems('filosofian-akatemia');
-    return faPublicItems.getNotes([{type: "keyword", include: "blogi"}]);
+    return filterBlogsFromPublicItems(faPublicItems);
+  }
+
+  function filterBlogsFromPublicItems(faPublicItems){
+    var unrenderedBlogs = faPublicItems.getNotes([{type: "keyword", include: "blogi"}]);
+    if (config.debug){
+      var unrenderedDevelopmentBlogs = faPublicItems.getNotes([{type: "keyword", include: "devblogi"}]);
+      return unrenderedDevelopmentBlogs.concat(unrenderedBlogs);
+    }else{
+      return unrenderedBlogs;
+    }
   }
 
   function renderBlogs(unrenderedBlogs) {
@@ -378,6 +519,9 @@ module.exports = (config, app, backendApi) => {
     let noteHtml = markdownParser.render(publicNote.content);
     let extractResult = extractLeadAndPictureAndContentFromHtml(noteHtml);
     blog.content = extractResult.content;
+    if ((blog.content.match(/<p>/g) || []).length < 4){
+      blog.lessThanFourParagraphs = true;
+    }
     blog.lead = extractResult.lead;
 
     if (extractResult.pictureData) {
@@ -451,12 +595,7 @@ module.exports = (config, app, backendApi) => {
   }
 
   function isAuthorTag(tag) {
-    if (tag.title === 'aleksej' || tag.title === 'emilia' || tag.title === 'frank' || tag.title === 'iida' ||
-        tag.title === 'joonas' || tag.title === 'jp' || tag.title === 'karoliina' || tag.title === 'lauri' ||
-        tag.title === 'maria' || tag.title === 'peter' || tag.title === 'reima' || tag.title === 'santeri' ||
-        tag.title === 'selina' || tag.title === 'sonja' || tag.title === 'tapani' || tag.title === 'timo' ||
-        tag.title === 'villiam')
-    {
+    if (getAuthorName(tag)){
       return true;
     }
   }
@@ -497,6 +636,8 @@ module.exports = (config, app, backendApi) => {
       return 'Timo Tiuraniemi';
       case 'villiam':
       return 'Villiam Virkkunen';
+      case 'fa':
+      return 'Filosofian Akatemia';
     }
   }
 
@@ -536,6 +677,8 @@ module.exports = (config, app, backendApi) => {
       return 'https://filosofianakatemia.fi/static/img/timo-large.jpg';
       case 'villiam':
       return 'https://filosofianakatemia.fi/static/img/villiam-large.jpg';
+      case 'fa':
+      return 'https://filosofianakatemia.fi/static/img/address-circle.png';
     }
   }
 
